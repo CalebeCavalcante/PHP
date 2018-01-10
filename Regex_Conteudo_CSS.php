7685d4fca2ced6edad4c98e7be7374dfb090d686
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2018-01-10 01:34:27 -0200

    Update Regex_Conteudo_CSS.php

--- Regex_Conteudo_CSS.php
+++ Regex_Conteudo_CSS.php
@@ -1,11 +1,11 @@
 <?php
-$regex = " / (\s+[\w|-]+:\s+[\w|\d|\#]+;) /";
+$regex = " / (\s+[\w|-]*:\s*[\w|\d|\#]+;) /";
 
 $str_css = " .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
 
 $retorno = " color: #979998; background-color: #979998; "; 
 
-$regex = "/ \.jr\-color\-gray\-6\s+\{(\s+[\w|-]+:\s+[\w|\d|\#]+;)+\} /  ";
+$regex = "/ \.jr\-color\-gray\-6\s*{(\s+[\w|-]*:\s*[\w|\d|\#]+;)+\s*} /  ";
 
 $retorno = " .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
 ?>
