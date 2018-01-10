00f84350c643137caeb51ccedfb69aceb2136c22
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2018-01-10 01:28:21 -0200

    Update Regex_Conteudo_CSS.php

--- Regex_Conteudo_CSS.php
+++ Regex_Conteudo_CSS.php
@@ -1,8 +1,11 @@
 <?php
 $regex = " / (\s+[\w|-]+:\s+[\w|\d|\#]+;) /";
 
-$str_css =" .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
+$str_css = " .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
 
-Retorno = " color: #979998; background-color: #979998; "; 
+$retorno = " color: #979998; background-color: #979998; "; 
 
+$regex = "/ \.jr\-color\-gray\-6\s+\{(\s+[\w|-]+:\s+[\w|\d|\#]+;)+\} /  ";
+
+$retorno = " .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
 ?>
