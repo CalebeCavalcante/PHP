a13c002dc5db58bd3c1333ed30829417bcf187d4
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2018-01-10 01:26:22 -0200

    Create Regex_Conteudo_CSS.php

---
+++ Regex_Conteudo_CSS.php
@@ -0,0 +1,8 @@
+<?php
+$regex = " / (\s+[\w|-]+:\s+[\w|\d|\#]+;) /";
+
+$str_css =" .jr-color-gray-6 {  color: #979998; background-color: #979998;} ";
+
+Retorno = " color: #979998; background-color: #979998; "; 
+
+?>
